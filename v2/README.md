ff215284f0f2db07af4cd995a4ed7b889daa82a2
Salvatore Foderaro <salvatore.foderaro@gmail.com>
2021-05-18 12:51:18 +0200

    17 - 18.05.2021

--- v2/README.md
+++ v2/README.md
@@ -2,18 +2,18 @@
 
 ## Graph of result
 
-<img src="img/Optimal and heuristic value(3).svg">
+<img src="img/Optimal and heuristic value(4).svg">
 
 <center><a href="https://docs.google.com/spreadsheets/d/e/2PACX-1vQkg3lZDbq69OKzgwoV0-PpzCSn5cElAXlBboKvxG6qR8mN_D1yzaxU2XemSzzOzNKafSmwPr8_cck9/pubchart?oid=1868444326&format=interactive">Link to the interactive graph</a></center>
 
 ---
 
-<img src="img/Optimal and heuristic value (log scale)(3).svg">
+<img src="img/Optimal and heuristic value (log scale)(4).svg">
 
 <center><a href="https://docs.google.com/spreadsheets/d/e/2PACX-1vQkg3lZDbq69OKzgwoV0-PpzCSn5cElAXlBboKvxG6qR8mN_D1yzaxU2XemSzzOzNKafSmwPr8_cck9/pubchart?oid=2130401916&format=interactive">Link to the interactive graph</a></center>
 
 ---
 
-<img src="img/Optimal and heuristic rem. stocks (log scale)(2).svg">
+<img src="img/Remain stocks value (log scale).svg">
 
-<center><a href="https://docs.google.com/spreadsheets/d/e/2PACX-1vQkg3lZDbq69OKzgwoV0-PpzCSn5cElAXlBboKvxG6qR8mN_D1yzaxU2XemSzzOzNKafSmwPr8_cck9/pubchart?oid=170011307&format=interactive">Link to the interactive graph</a></center>
+<center><a href="https://docs.google.com/spreadsheets/d/e/2PACX-1vQkg3lZDbq69OKzgwoV0-PpzCSn5cElAXlBboKvxG6qR8mN_D1yzaxU2XemSzzOzNKafSmwPr8_cck9/pubchart?oid=72686607&format=interactive">Link to the interactive graph</a></center>
--- v2/project_v2.py
+++ v2/project_v2.py
@@ -13,7 +13,7 @@ CSV_OUTPUT_FOLDER = "csv_solution_v2"
 T = 180
 NUMBER_OF_ELEMENT = 1000
 LIMIT_CAPACITY = 8
-INCREMENT = 0.1
+INCREMENT = 0.2
 
 def optimize_test_capacity_multiple_vaccines(T, B, Delta, Capacity):
 
@@ -89,7 +89,15 @@ def add_column_to_df(df, values_list, new_column_name):
     df[new_column_name] = values_list
     return df
 
-def heuristic_v2(b_list, capacity):
+def heuristic_v2_sum(first_doses_list, delta, t, l):
+    value_sum = 0
+
+    for i in range(1, l+1):
+        if t + i - delta >= 0 and t + i - delta <= 180:
+            value_sum += first_doses_list[t + i - delta]
+    return value_sum
+
+def heuristic_v2_risk(b_list, capacity, l):
 
     vaccines = { "Pfizer": [21, [0]*180, [0]*180], "Moderna": [28, [0]*180, [0]*180], "Astrazeneca": [78, [0]*180, [0]*180] }
     stocks = []
@@ -104,6 +112,7 @@ def heuristic_v2(b_list, capacity):
         index = 0
         remain_capacity = capacity
         for u in vaccines:
+            
             delta = vaccines[u][0]
             first_doses = vaccines[u][1]
             stocks = vaccines[u][2]
@@ -113,35 +122,46 @@ def heuristic_v2(b_list, capacity):
 
                 if t == 0:
                     first_doses_somministrated = arrival[t]
-                    first_doses_somministrated = int(first_doses_somministrated/2)
                 elif t-delta < 0:
                     first_doses_somministrated = stocks[t-1] + arrival[t]
-                    first_doses_somministrated = int(first_doses_somministrated/2)
-                elif t-delta >= 0 and t+1-delta < 0:
-                    first_doses_somministrated = stocks[t-1] - first_doses[t-delta] + arrival[t]
                 else:
-                    first_doses_somministrated = stocks[t-1] - first_doses[t-delta] - first_doses[t+1-delta] + arrival[t]
+                    first_doses_somministrated = stocks[t-1] - first_doses[t-delta] - heuristic_v2_sum(first_doses, delta, t, l) + arrival[t]
 
                 if first_doses_somministrated < 0:
                     first_doses_somministrated = 0
 
+                if first_doses_somministrated == 0:
+                    fraction_of_capacity = 0
+                else:
+                    if first_doses_somministrated / remain_capacity > 1:
+                        fraction_of_capacity = remain_capacity / first_doses_somministrated
+                    else:
+                        fraction_of_capacity = first_doses_somministrated / remain_capacity
+
                 if index == 0:
-                    first_doses_somministrated = min(first_doses_somministrated, capacity * 0.33)
+                    if (fraction_of_capacity > 0.33):
+                        first_doses_somministrated = min(first_doses_somministrated, capacity * 0.33)
+                    else:
+                        first_doses_somministrated = min(first_doses_somministrated, capacity * fraction_of_capacity)
                 elif index == 1:
-                    first_doses_somministrated = min(first_doses_somministrated, capacity * 0.33)
+                    if (fraction_of_capacity > 0.5):
+                        first_doses_somministrated = min(first_doses_somministrated, capacity * 0.5)
+                    else:
+                        first_doses_somministrated = min(first_doses_somministrated, capacity * fraction_of_capacity)
                 elif index == 2:
-                    first_doses_somministrated = min(first_doses_somministrated, capacity * 0.33)
+                    first_doses_somministrated = min(first_doses_somministrated, remain_capacity)
+                
+                # first_doses_somministrated = min(first_doses_somministrated, capacity * 0.33)
 
                 total_second_doses += first_doses_somministrated
                 object_function += (t+1+delta)*first_doses_somministrated
                 vaccines[u][1][t] = first_doses_somministrated
+                remain_capacity -= first_doses_somministrated
                 
                 if t == 0:
                     vaccines[u][2][t] = - first_doses_somministrated  + arrival[t]
                 elif t - delta < 0:
                     vaccines[u][2][t] = stocks[t-1] - first_doses_somministrated + arrival[t]
-                elif t - delta >= 0 and t+1-delta < 0:
-                    vaccines[u][2][t] = stocks[t-1] - first_doses_somministrated - first_doses[t-delta] + arrival[t]
                 else:
                     vaccines[u][2][t] = stocks[t-1] - first_doses_somministrated - first_doses[t-delta] + arrival[t]
             else:
@@ -155,19 +175,116 @@ def heuristic_v2(b_list, capacity):
 
     return ([object_function/total_second_doses, sum_penality])
 
+def heuristic_v2(b_list, capacity):
+
+    vaccines = { "Pfizer": [21, [0]*180, [0]*180], "Moderna": [28, [0]*180, [0]*180], "Astrazeneca": [78, [0]*180, [0]*180] }
+    stocks = []
+
+    object_function = 0 
+    total_second_doses = 0
+    index = 0
+    sum_penality = 0
+
+    for t in range(0, 180):
+        
+        index = 0
+        remain_capacity = capacity
+
+        for u in vaccines:
+            delta = vaccines[u][0]
+            first_doses = vaccines[u][1]
+            stocks = vaccines[u][2]
+            arrival = b_list[u]
+
+            if t + delta < 180:
+
+                if t == 0:
+                    first_doses_somministrated = arrival[t]
+                elif t-delta < 0:
+                    first_doses_somministrated = stocks[t-1] + arrival[t]
+                else:
+                    first_doses_somministrated = stocks[t-1] - first_doses[t-delta] + arrival[t]
+
+                first_doses_somministrated = int(first_doses_somministrated / 2)
+
+                if first_doses_somministrated == 0:
+                    fraction_of_capacity = 0
+                else:
+                    if first_doses_somministrated / remain_capacity > 1:
+                        fraction_of_capacity = remain_capacity / first_doses_somministrated
+                    else:
+                        fraction_of_capacity = first_doses_somministrated / remain_capacity
+                
+                if index == 0:
+                    if (fraction_of_capacity > 0.33):
+                        first_doses_somministrated = min(first_doses_somministrated, capacity * 0.33)
+                    else:
+                        first_doses_somministrated = min(first_doses_somministrated, capacity * fraction_of_capacity)
+                elif index == 1:
+                    if (fraction_of_capacity > 0.5):
+                        first_doses_somministrated = min(first_doses_somministrated, capacity * 0.5)
+                    else:
+                        first_doses_somministrated = min(first_doses_somministrated, capacity * fraction_of_capacity)
+                elif index == 2:
+                    first_doses_somministrated = min(first_doses_somministrated, remain_capacity)
+
+                # first_doses_somministrated = min(first_doses_somministrated, capacity * 0.33)
+
+                total_second_doses += first_doses_somministrated
+                object_function += (t+1+delta)*first_doses_somministrated
+                vaccines[u][1][t] = first_doses_somministrated
+                remain_capacity -= first_doses_somministrated                
+
+                if t == 0:
+                    vaccines[u][2][t] = - first_doses_somministrated - first_doses_somministrated + arrival[t]
+                else:
+                    vaccines[u][2][t] = stocks[t-1] - first_doses_somministrated - first_doses_somministrated  + arrival[t]
+
+            else:
+                vaccines[u][2][t] = stocks[t-1] + arrival[t]
+
+            index += 1
+
+    sum_penality += vaccines["Pfizer"][2][180-1] 
+    sum_penality += vaccines["Astrazeneca"][2][180-1] 
+    sum_penality += vaccines["Moderna"][2][180-1]
+
+    return ([object_function/total_second_doses, sum_penality])
+
 if __name__ == "__main__":
     
     penality_optimal_result = {}
     optimal_result = {}
+    
     heuristic_result = {}
+    heuristic_risk_result = {}
+    heuristic_risk_result_7 = {}
+    heuristic_risk_result_14 = {}
+    heuristic_risk_result_21 = {}
+
     penality_heuristic = {}
+    penality_heuristic_risk = {}
+    penality_heuristic_risk_7 = {}
+    penality_heuristic_risk_14 = {}
+    penality_heuristic_risk_21 = {}
 
     num = 1
     while(num < LIMIT_CAPACITY):
         penality_optimal_result[str(num) + " c"] = []
         optimal_result[str(num) + " c"] = []
+        
         heuristic_result[str(num) + " c"] = []
+        heuristic_risk_result[str(num) + " c"] = []
+        heuristic_risk_result_7[str(num) + " c"] = []
+        heuristic_risk_result_14[str(num) + " c"] = []
+        heuristic_risk_result_21[str(num) + " c"] = []
+
         penality_heuristic[str(num) + " c"] = []
+        penality_heuristic_risk[str(num) + " c"] = []
+        penality_heuristic_risk_7[str(num) + " c"] = []
+        penality_heuristic_risk_14[str(num) + " c"] = []
+        penality_heuristic_risk_21[str(num) + " c"] = []
+
         num += INCREMENT
         num = round(num, 2)
 
@@ -210,7 +327,11 @@ if __name__ == "__main__":
 
             result = optimize_test_capacity_multiple_vaccines(len(b_list_0), b_list, DELTA, capacity[u])
             heu_result = heuristic_v2(b_list, capacity[u])
-            
+            heu_result_risk = heuristic_v2_risk(b_list, capacity[u], 1)
+            heu_result_risk_7 = heuristic_v2_risk(b_list, capacity[u], 7)
+            heu_result_risk_14 = heuristic_v2_risk(b_list, capacity[u], 14)
+            heu_result_risk_21 = heuristic_v2_risk(b_list, capacity[u], 21)
+
             # Variable to get the penality for the optimal solution
             second_doses_sum = 0
             penality_sum = 0
@@ -234,7 +355,19 @@ if __name__ == "__main__":
             # Calculate the heuristic result
             heuristic_result[u].append( heu_result[0] )
             penality_heuristic[u].append( heu_result[1] )
-        
+
+            heuristic_risk_result[u].append( heu_result_risk[0] )
+            penality_heuristic_risk[u].append( heu_result_risk[1] )
+
+            heuristic_risk_result_7[u].append( heu_result_risk_7[0] )
+            penality_heuristic_risk_7[u].append( heu_result_risk_7[1] )
+
+            heuristic_risk_result_14[u].append( heu_result_risk_14[0] )
+            penality_heuristic_risk_14[u].append( heu_result_risk_14[1] )
+
+            heuristic_risk_result_21[u].append( heu_result_risk_21[0] )
+            penality_heuristic_risk_21[u].append( heu_result_risk_21[1] )
+
             # Write the solution to the single file (create direcitory if not exists)
             output_dir = Path("csv_solution_v2/capacity_" + u)
             output_dir.mkdir(parents=True, exist_ok=True)
@@ -249,38 +382,107 @@ if __name__ == "__main__":
 
     avg_optimal_value = []
     avg_heuristic_value = []
+    avg_heuristic_risk_value = []
+    avg_heuristic_risk_value_7 = []
+    avg_heuristic_risk_value_14 = []
+    avg_heuristic_risk_value_21 = []
+
     avg_result_difference = []
+    avg_result_difference_risk = []
+    avg_result_difference_risk_7 = []
+    avg_result_difference_risk_14 = []
+    avg_result_difference_risk_21 = []
 
     avg_stocks_optimal = []
     avg_stocks_heuristic = []
+    avg_stocks_risk = []
+    avg_stocks_risk_7 = []
+    avg_stocks_risk_14 = []
+    avg_stocks_risk_21 = []
+
     avg_stocks_difference = []
+    avg_stocks_difference_risk = []
+    avg_stocks_difference_risk_7 = []
+    avg_stocks_difference_risk_14 = []
+    avg_stocks_difference_risk_21 = []
 
     for k in optimal_result:
         df[k + " - optimal solution"] = optimal_result[k]
         df[k + " - optimal remain stocks"] = penality_optimal_result[k]
+
         df[k + " - heuristic solution"] = heuristic_result[k]
         df[k + " - heuristic remain stocks"] = penality_heuristic[k]
+
+        df[k + " - heuristic q-1 risk solution"] = heuristic_risk_result[k]
+        df[k + " - heuristic q-1 risk remain stocks"] = penality_heuristic_risk[k]
+
+        df[k + " - heuristic q-7 risk solution"] = heuristic_risk_result_7[k]
+        df[k + " - heuristic q-7risk remain stocks"] = penality_heuristic_risk_7[k]
+
+        df[k + " - heuristic q14 risk solution"] = heuristic_risk_result_14[k]
+        df[k + " - heuristic q-14 risk remain stocks"] = penality_heuristic_risk_14[k]
+
+        df[k + " - heuristic q-21 risk solution"] = heuristic_risk_result_21[k]
+        df[k + " - heuristic q-21 risk remain stocks"] = penality_heuristic_risk_21[k]
         
         capacity_list.append(k)
 
         avg_optimal_value.append(round( mean(optimal_result[k] ), 2))
         avg_heuristic_value.append(round( mean(heuristic_result[k] ), 2))
+        avg_heuristic_risk_value.append(round( mean(heuristic_risk_result[k] ), 2))
+        avg_heuristic_risk_value_7.append(round( mean(heuristic_risk_result_7[k] ), 2))
+        avg_heuristic_risk_value_14.append(round( mean(heuristic_risk_result_14[k] ), 2))
+        avg_heuristic_risk_value_21.append(round( mean(heuristic_risk_result_21[k] ), 2))
+
         avg_result_difference.append(round( (mean(heuristic_result[k]) - mean(optimal_result[k])) / mean(heuristic_result[k]), 4))
+        avg_result_difference_risk.append(round( (mean(heuristic_risk_result[k]) - mean(optimal_result[k])) / mean(heuristic_risk_result[k]), 4))
+        avg_result_difference_risk_7.append(round( (mean(heuristic_risk_result_7[k]) - mean(optimal_result[k])) / mean(heuristic_risk_result_7[k]), 4))
+        avg_result_difference_risk_14.append(round( (mean(heuristic_risk_result_14[k]) - mean(optimal_result[k])) / mean(heuristic_risk_result_14[k]), 4))
+        avg_result_difference_risk_21.append(round( (mean(heuristic_risk_result_21[k]) - mean(optimal_result[k])) / mean(heuristic_risk_result_21[k]), 4))
 
         avg_stocks_optimal.append(round( mean(penality_optimal_result[k] ), 2))
         avg_stocks_heuristic.append(round( mean(penality_heuristic[k] ), 2))
+        avg_stocks_risk.append(round( mean(penality_heuristic_risk[k] ), 2))
+        avg_stocks_risk_7.append(round( mean(penality_heuristic_risk_7[k] ), 2))
+        avg_stocks_risk_14.append(round( mean(penality_heuristic_risk_14[k] ), 2))
+        avg_stocks_risk_21.append(round( mean(penality_heuristic_risk_21[k] ), 2))
+
         avg_stocks_difference.append(round( (mean(penality_heuristic[k]) - mean(penality_optimal_result[k])) / mean(penality_heuristic[k]), 4))
+        avg_stocks_difference_risk.append(round( (mean(penality_heuristic_risk[k]) - mean(penality_optimal_result[k])) / mean(penality_heuristic_risk[k]), 4))
+        avg_stocks_difference_risk_7.append(round( (mean(penality_heuristic_risk_7[k]) - mean(penality_optimal_result[k])) / mean(penality_heuristic_risk_7[k]), 4))
+        avg_stocks_difference_risk_14.append(round( (mean(penality_heuristic_risk_14[k]) - mean(penality_optimal_result[k])) / mean(penality_heuristic_risk_14[k]), 4))
+        avg_stocks_difference_risk_21.append(round( (mean(penality_heuristic_risk_21[k]) - mean(penality_optimal_result[k])) / mean(penality_heuristic_risk_21[k]), 4))
 
     df.to_csv("result_v2.csv", index=0)
 
     df = pd.DataFrame(capacity_list, columns= ['Capacity'])
 
     df['Optimal value'] = avg_optimal_value
+    
     df['Heuristic value'] = avg_heuristic_value
+    df['Heuristic risk value'] = avg_heuristic_risk_value
+    df['Heuristic 7 risk value'] = avg_heuristic_risk_value_7
+    df['Heuristic 14 risk value'] = avg_heuristic_risk_value_14
+    df['Heuristic 21 risk value'] = avg_heuristic_risk_value_21
+    
     df['Result difference'] = avg_result_difference
+    df['Heuristic q-1 risk difference'] = avg_result_difference_risk
+    df['Heuristic q-7 risk difference'] = avg_result_difference_risk_7
+    df['Heuristic q-14 risk difference'] = avg_result_difference_risk_14
+    df['Heuristic q-21 risk difference'] = avg_result_difference_risk_21
 
     df['Optimal rem. stocks'] = avg_stocks_optimal
+    
     df['Heuristic rem. stocks'] = avg_stocks_heuristic
+    df['Heuristic q-1 risk rem.'] = avg_stocks_risk
+    df['Heuristic q-7 risk rem.'] = avg_stocks_risk_7
+    df['Heuristic q-14 risk rem.'] = avg_stocks_risk_14
+    df['Heuristic q-21 ris rem. stocks'] = avg_stocks_risk_21
+
     df['Stocks difference'] = avg_stocks_difference
+    df['Stocks q-1 risk difference'] = avg_stocks_difference_risk
+    df['Stocks q-7 risk difference'] = avg_stocks_difference_risk_7
+    df['Stocks q-14 risk difference'] = avg_stocks_difference_risk_14
+    df['Stocks q-21 risk difference'] = avg_stocks_difference_risk_21
 
     df.to_csv("result_summary_v2.csv", index = 0)
